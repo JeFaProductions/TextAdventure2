4cdfcb1543f12f119f8884016cda2c855f37abae
JeGa <jega@win>
2016-09-14 22:15:05 +0200

    Added enum.

--- tead/game.py
+++ tead/game.py
@@ -1,32 +1,18 @@
+import enum
 import tead.event as evt
 
-NORTH = 0
-SOUTH = 1
-EAST = 2
-WEST = 3
+_DIRECTION = enum.Enum(
+    "_DIRECTION", "NORTH, SOUTH, EAST, WEST"
+)
 
-_DIR_TO_STR = ['NORTH', 'SOUTH', 'EAST', 'WEST']
-_DIR_FROM_STR = {'NORTH' : 0, 'SOUTH' : 1, 'EAST': 2, 'WEST' : 3}
-
-def dirToStr(direction):
-    if direction < 0 or direction > 3:
-        return None
-    return _DIR_TO_STR[direction]
-
-def dirFromStr(directionStr):
-    upStr = directionStr.upper()
-    if not upStr in _DIR_FROM_STR:
-        return None
-    return _DIR_FROM_STR[upStr]
 
 class Item:
-
     def __init__(self):
         self.id = ''
         self.name = ''
 
-class Player:
 
+class Player:
     def __init__(self):
         self.inventory = dict()
 
@@ -40,17 +26,21 @@ class Player:
 
 
 class Door:
-
     def __init__(self, locked=False):
         self.locked = locked
         self.nextRoom = ''
 
-class Room:
 
+class Room:
     def __init__(self):
         self.id = ''
         self.name = ''
-        self.doors = [None, None, None, None]
+        self.doors = {
+            _DIRECTION.NORTH: None,
+            _DIRECTION.EAST: None,
+            _DIRECTION.SOUTH: None,
+            _DIRECTION.WEST: None
+        }
         self.items = dict()
         self._eventListener = []
 
@@ -63,13 +53,13 @@ class Room:
         return item
 
     def hasDoor(self, direction):
-        return direction >= 0 and direction < len(self.doors) and not self.doors[direction] is None
+        return self.doors[direction] is not None
 
     def canPassDoor(self, direction):
         return self.hasDoor(direction) and not self.doors[direction].locked
 
-class World:
 
+class World:
     def __init__(self, eventSystem):
         self.currentRoomID = ''
         self.player = Player()
@@ -80,16 +70,17 @@ class World:
         self.rooms[room.id] = room
 
     def gotoDirectionStr(self, directionStr):
-        direction = dirFromStr(directionStr)
-
-        if direction is None:
+        directionStr = directionStr.upper()
+        if directionStr not in _DIRECTION.__members__:
             # TODO print 'invalid direction'
             return
+
+        direction = _DIRECTION[directionStr]
 
         self.gotoDirection(direction)
 
     def gotoDirection(self, direction):
-        assert(self.currentRoomID in self.rooms)
+        assert (self.currentRoomID in self.rooms)
 
         currRoom = self.rooms[self.currentRoomID]
 
@@ -103,4 +94,4 @@ class World:
 
         self.currentRoomID = currRoom.doors[direction].nextRoom
         self.eventSystem.createEvent(evt.Event(evt.ROOM_ENTERED,
-           {'room' : self.rooms[self.currentRoomID]}))
+                                               {'room': self.rooms[self.currentRoomID]}))
